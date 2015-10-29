fe6ee870144a8a4366386525bfcdd8ab8c7785ba
orangetest <ivan.dikov@orangedot.bg>
2015-10-29 17:25:24 +0200

    create git

---
+++ GitA1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A1
+        }
+    }
+}
